a0d72281abcabdb6f0cdb306f56d14b1241fb4cd
SuzukiKensuke <s1811349@s.tsukuba.ac.jp>
2021-01-10 18:53:54 +0900

    update

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+
+#include <cstdlib>
 using namespace std;
 using namespace std::this_thread;
 using namespace std::chrono;
@@ -10,18 +12,52 @@ typedef long double ld;
 const double PI = acos(-1);
 #define all(n) n.begin(), n.end()
 
-void init() { cout << "init" << endl; }
+vector<vector<bool>> g;
+int n;  // grid size
+int step;
+
+void init() {
+  cout << "init" << endl;
+  g.resize(n + 2, vector<bool>(n + 2, true));
+  rep(i, n + 2) {
+    rep(j, n + 2) {}
+  }
+}
 
 void update() { cout << "update" << endl; }
 
-void display() { cout << "display" << endl; }
+void display() {
+  cout << "display" << endl;
+  for (int i = 0; i < n + 2; ++i) {
+    for (int j = 0; j < n + 2; ++j) {
+      if ((i == 0 || i == n + 1) && (j == 0 || j == n + 1))
+        cout << "+";
+      else if (i == 0 || i == n + 1)
+        cout << "-";
+      else if (j == 0 || j == n + 1)
+        cout << "|";
+      else
+        cout << (g[i][j] ? "*" : " ");
+    }
+    printf("\n");
+  }
+}
 
-int main() {
-  int step = 5;
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    cout << "usage: ./a.out grid_size step" << endl;
+    return 0;
+  }
+
+  n = stoi(argv[1]);
+  step = stoi(argv[2]);
+
   init();
   while (step--) {
+    system("clear");  // コンソールをクリア
     update();
     display();
-    sleep_for(milliseconds(100));
+    cout << step << endl;
+    sleep_for(milliseconds(500));
   }
 }
