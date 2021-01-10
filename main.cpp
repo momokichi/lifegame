6a92c44edc2de4377a8b4a0355679a2f4e01c48e
SuzukiKensuke <s1811349@s.tsukuba.ac.jp>
2021-01-11 00:39:00 +0900

    complete

--- main.cpp
+++ main.cpp
@@ -26,9 +26,39 @@ void init() {
     for (int j = 1; j < n + 1; ++j) g[i][j] = my_rand(mt);
 }
 
+bool update_cell(bool current, int count) {
+  if (!current && count == 3)
+    return true;
+  else if (current && (count == 2 || count == 3))
+    return true;
+  else if (current && count <= 1)
+    return false;
+  else if (current && count >= 4)
+    return false;
+  else
+    return false;
+}
+
+const int dx[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+const int dy[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+
+int count_alive_cells(int i, int j) {
+  int ret = 0;
+  for (int direction = 0; direction < 8; ++direction) {
+    if (g[i + dx[direction]][j + dy[direction]]) ++ret;
+  }
+  return ret;
+}
+
 void update() {
+  vector<vector<bool>> tmp(n + 2, vector<bool>(n + 2, false));
   for (int i = 1; i < n + 1; ++i)
-    for (int j = 1; j < n + 1; ++j) g[i][j] = my_rand(mt);
+    for (int j = 1; j < n + 1; ++j) {
+      int count = count_alive_cells(i, j);
+      // cout << count << endl;
+      tmp[i][j] = update_cell(g[i][j], count);
+    }
+  g = tmp;
 }
 
 void display() {
@@ -62,7 +92,7 @@ int main(int argc, char *argv[]) {
     system("clear");  // clear console
     update();
     display();
-    sleep_for(milliseconds(500));  // sleep
+    sleep_for(milliseconds(200));  // sleep
   }
   system("clear");
   return 0;
